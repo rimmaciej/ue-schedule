bd38124bb55822eb3c55aa67077350f4995a860d
Maciej Rim <maciej.rim@gmail.com>
2019-10-07 17:04:50 +0200

    group events by date, move plan id and date range to a separate config file

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import re
 import requests
 from icalendar import Calendar, Event
 
-# Timetable download url
-url = ""
+from config import PLAN_ID, START_DATE, END_DATE
+
+# Prepare the url
+url = f"https://e-uczelnia.ue.katowice.pl/wsrest/rest/ical/phz/calendarid_{PLAN_ID}.ics"
+if START_DATE and END_DATE:
+    url += f"?dataod={START_DATE}&datado={END_DATE}"
 
 # Regex that matches group string
 grp_regex = re.compile(r"\w{1,}_K-ce_19_z_SI_.*(,)?")
@@ -36,6 +40,20 @@ events = [
     if component.name == "VEVENT"
 ]
 
-events = sorted(events, key=lambda event: (event["summary"], event["dtstart"]))
+events = sorted(events, key=lambda event: event["dtstart"])
+
+events_by_date = {}
+
 for event in events:
-    print(f"{event['dtstart']} - {event['summary']} - {event['location']}")
+    ev_date = event["dtstart"].strftime("%Y-%m-%d")
+
+    if ev_date not in events_by_date.keys():
+        events_by_date[ev_date] = []
+    events_by_date[ev_date].append(event)
+
+for date, events in events_by_date.items():
+    print(date)
+    for event in events:
+        start_time = event["dtstart"].strftime("%H:%M")
+        print(f"\t{start_time} {event['summary']} - {event['location']}")
+    print()
