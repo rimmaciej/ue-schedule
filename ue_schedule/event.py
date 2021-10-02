d6df9ddce66e27a0082e26517f453b06a2f381ba
Maciej Rim <maciej.rim@gmail.com>
2021-10-02 13:46:10 +0200

    Restructure and add json (de)serialization (#10)

--- ue_schedule/event.py
+++ ue_schedule/event.py
@@ -2,7 +2,7 @@ import re
 from datetime import datetime, timedelta
 from typing import Optional
 
-from icalendar import Event as CalEvent
+from icalendar import Event as CalEvent  # type: ignore
 from pytz import timezone
 
 
@@ -11,65 +11,85 @@ class Event:
     Class representing a single class schedule Event
     """
 
-    def __init__(self, component: CalEvent) -> None:
+    name: str
+    start: datetime
+    end: datetime
+    type: Optional[str]
+    teacher: Optional[str]
+    location: Optional[str]
+
+    def __init__(
+        self,
+        name: str,
+        type: Optional[str],
+        teacher: Optional[str],
+        location: Optional[str],
+        start: datetime,
+        end: datetime,
+    ):
+        self.name = name
+        self.type = type
+        self.teacher = teacher
+        self.start = start
+        self.end = end
+        self.location = location
+
+    @classmethod
+    def from_calendar(cls, component: CalEvent) -> "Event":
         """
         Initialize Event object
 
         :param component: calendar event component
         """
-
-        self.name: str = ""
-        self.type: Optional[str] = None
-        self.teacher: Optional[str] = None
-
         # Get location from the calendar event replace @ with CNTI, set as None if no location
-        self.location: Optional[str] = str(component.get("location")).strip()
-        self.location = self.location.replace("@", "CNTI")
-        self.location = None if self.location == "brak lokalizacji brak sali" else self.location
+        location: Optional[str] = str(component.get("location")).strip()
+        if location:
+            location = location.replace("@", "CNTI")
+        location = None if location == "brak lokalizacji brak sali" else location
 
         # Normalize start and end time to Europe/Warsaw timezone
         tz = timezone("Europe/Warsaw")
-        self.start: datetime = tz.normalize(tz.localize(component.get("dtstart").dt))
-        self.end: datetime = tz.normalize(tz.localize(component.get("dtend").dt))
+        start: datetime = tz.normalize(tz.localize(component.get("dtstart").dt))
+        end: datetime = tz.normalize(tz.localize(component.get("dtend").dt))
 
         # fix the finish time of 1.5h or 2.25h events
         # subtract 10 min break pointlessly included in original schedule
-        duration = self.end - self.start
+        duration = end - start
         if duration == timedelta(minutes=100) or duration == timedelta(minutes=155):
-            self.end -= timedelta(minutes=10)
+            end -= timedelta(minutes=10)
 
-        self._parse_summary(str(component.get("summary")).strip())
-
-    def _parse_summary(self, summary: str):
         # remove groups from summary
-        summary = re.sub(r"\w{1,}_K-ce.*(,)?", "", summary)
+        summary = re.sub(r"\w{1,}_K-ce.*(,)?", "", str(component.get("summary")).strip())
 
         # split summary into name and teacher
         split_summary = summary.strip().split("  ")
 
-        if len(split_summary) > 1:
-            self.teacher = split_summary.pop().strip()
+        teacher: Optional[str]
 
-            if self.teacher.startswith("_K-ce"):
-                self.teacher = split_summary.pop().strip()
+        if len(split_summary) > 1:
+            teacher = split_summary.pop().strip()
+
+            if teacher.startswith("_K-ce"):
+                teacher = split_summary.pop().strip()
 
             # set teacher to none if not specified
-            self.teacher = None if self.teacher == "brak nauczyciela" else self.teacher
-
-            self.name = (" ".join(split_summary)).strip()
+            teacher = None if teacher == "brak nauczyciela" else teacher
+            name = (" ".join(split_summary)).strip()
         else:
-            self.teacher = None
-            self.name = split_summary[0]
+            teacher = None
+            name = split_summary[0]
 
-        self.name = self.name.replace("brak nauczyciela", "").replace(
-            "brak lokalizacji brak sali", ""
-        )
+        name = name.replace("brak nauczyciela", "").replace("brak lokalizacji brak sali", "")
 
         # split out event type from name
-        split_name = self.name.strip().split(" - ")
+        split_name = name.strip().split(" - ")
+
+        type: Optional[str]
 
         if len(split_name) > 1:
-            self.name = split_name[0].strip()
-            self.type = split_name[1].strip()
+            name = split_name[0].strip()
+            type = split_name[1].strip()
         else:
-            self.type = None
+            type = None
+
+        return cls(name, type, teacher, location, start, end)
