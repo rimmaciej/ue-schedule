eb2517e5b3d0745ec31e89322a9d534b4d2f7f70
Maciej Rim <maciej.rim@gmail.com>
2021-10-16 19:17:18 +0200

    Fix fetching for some IDs (#26)
    - rework the parsing algorithm
    - use an Enum for event types

--- ue_schedule/cli.py
+++ ue_schedule/cli.py
@@ -3,19 +3,37 @@ from typing import Optional
 
 import click  # type: ignore
 
+from .event import EventType
 from .schedule import Schedule
 
 today = datetime.now().date()
+
+event_types = {
+    EventType.Wyklad: click.style("Wykład", fg="green"),
+    EventType.Cwiczenia: click.style("Ćwiczenia", fg="red"),
+    EventType.Laboratorium: click.style("Laboratorium", fg="magenta"),
+    EventType.Lektorat: click.style("Lektorat", fg="blue"),
+    EventType.Seminarium: click.style("Seminarium", fg="cyan"),
+    EventType.WF: click.style("WF", fg="yellow"),
+    EventType.Inny: click.style("Inny", fg="bright_black"),
+}
 
 
 @click.command()
 @click.argument("schedule_id", type=str)
 @click.option("-j", "--json", is_flag=True, default=False, show_default=True)
+@click.option("-g", "--groups", is_flag=True, default=False, show_default=True)
 @click.option(
     "-s", "--start", "start_date", type=click.DateTime(), default=str(today), show_default=True
 )
 @click.option("-e", "--end", "end_date", type=click.DateTime())
-def main(schedule_id: str, json: bool, start_date: datetime, end_date: Optional[datetime] = None):
+def main(
+    schedule_id: str,
+    json: bool,
+    groups: bool,
+    start_date: datetime,
+    end_date: Optional[datetime] = None,
+) -> None:
     schedule = Schedule(schedule_id)
 
     if not end_date:
@@ -24,30 +42,31 @@ def main(schedule_id: str, json: bool, start_date: datetime, end_date: Optional[
     events = schedule.get_events(start_date.date(), end_date.date())
 
     if json:
-        print(schedule.get_json(start_date, end_date))
+        print(Schedule.format_as_json(events))
         return
 
     response = ""
 
     for day in events:
-        response += click.style(f"{day['date']}\n", fg="green")
 
         if len(day["events"]) == 0:
-            response += "  No events for this day\n"
+            response += click.style(f"{day['date']} - brak zajęć\n", fg="bright_black")
+        else:
+            response += click.style(f"{day['date']}\n", fg="green")
 
         for event in day["events"]:
             event_time = click.style(f"{event.start:%H:%M} - {event.end:%H:%M}", fg="yellow")
 
-            if event.type == "Lektorat":
-                event_type = click.style(event.type, fg="blue")
-            elif event.type == "Ćwiczenia":
-                event_type = click.style(event.type, fg="red")
-            elif event.type.startswith("wykład"):
-                event_type = click.style("Wykład", fg="green")
-            else:
-                event_type = click.style(event.type, fg="bright_black")
+            teacher = click.style(
+                event.teacher if event.teacher else "Nieznany nauczyciel", fg="bright_black"
+            )
 
-            response += f"  {event_time}  {event.name} - {event_type} \n"
+            response += f"  {event_time}  {event.name} - {event_types[event.type]}\n"
+            response += f"                 {teacher}\n"
+
+            if groups:
+                groups = click.style(", ".join(event.groups), fg="bright_black")
+                response += f"                 {groups}\n"
 
         response += "\n"
 
--- ue_schedule/event.py
+++ ue_schedule/event.py
@@ -1,12 +1,23 @@
 import re
 from dataclasses import dataclass, field
 from datetime import datetime, timedelta
+from enum import Enum
 from typing import List, Optional
 
 from icalendar import Event as CalEvent  # type: ignore
 from pytz import timezone
 
-GROUP_REGEX = re.compile(r"(\w{1,}_K-ce.*,?)")
+GROUP_REGEX = re.compile(r"([A-Z]*_K-ce.*,?)")
+
+
+class EventType(Enum):
+    Wyklad = 1
+    Cwiczenia = 2
+    Laboratorium = 3
+    Lektorat = 4
+    Seminarium = 5
+    WF = 6
+    Inny = 7
 
 
 @dataclass(frozen=True, order=True)
@@ -18,7 +29,7 @@ class Event:
     name: str
     start: datetime
     end: datetime
-    type: Optional[str] = None
+    type: EventType
     teacher: Optional[str] = None
     location: Optional[str] = None
     groups: List[str] = field(default_factory=list)
@@ -33,6 +44,7 @@ class Event:
 
         :returns: Event instance
         """
+
         # Get location from the calendar event replace @ with CNTI, set as None if no location
         location: Optional[str] = str(component.get("location")).strip()
         if location:
@@ -51,45 +63,46 @@ class Event:
             if duration == timedelta(minutes=100) or duration == timedelta(minutes=155):
                 end -= timedelta(minutes=10)
 
-        # extract groups from summary
-        _summary = str(component.get("summary")).strip()
-        summary = re.sub(GROUP_REGEX, "", _summary)
+        teacher: Optional[str] = None
 
+        # extract summary
+        _summary = str(component.get("summary"))
+
+        # extract groups
         _groups = re.search(GROUP_REGEX, _summary)
+        summary = re.sub(GROUP_REGEX, "", _summary).strip()
         if _groups:
             groups = [group.strip() for group in _groups[0].split(",")]
         else:
             groups = []
 
-        # split summary into name and teacher
-        split_summary = summary.strip().split("  ")
+        # remove 'brak nauczyciela' and mark teacher as None
+        split = re.split(" - |  ", summary)
 
-        teacher: Optional[str]
+        if len(split) >= 3:
+            teacher = split.pop().strip()
+            _event_type = split.pop().strip().lower()
+            name = " ".join(split).strip()
 
-        if len(split_summary) > 1:
-            teacher = split_summary.pop().strip()
+            # Assign enum event types
+            if "wykład" in _event_type:
+                event_type = EventType.Wyklad
+            elif "ćwiczenia" in _event_type:
+                event_type = EventType.Cwiczenia
+            elif "lab" in _event_type:
+                event_type = EventType.Laboratorium
+            elif "lektorat" in _event_type:
+                event_type = EventType.Lektorat
+            elif "wf" in _event_type:
+                event_type = EventType.WF
+            elif "seminarium" in name.lower():
+                event_type = EventType.Seminarium
+            else:
+                event_type = EventType.Inny
 
-            if teacher.startswith("_K-ce"):
-                teacher = split_summary.pop().strip()
-
-            # set teacher to none if not specified
-            teacher = None if teacher == "brak nauczyciela" else teacher
-            name = (" ".join(split_summary)).strip()
-        else:
+        elif summary.endswith("brak nauczyciela"):
             teacher = None
-            name = split_summary[0]
+            event_type = EventType.Inny
+            name = summary.removesuffix("brak nauczyciela")
 
-        name = name.replace("brak nauczyciela", "").replace("brak lokalizacji brak sali", "")
-
-        # split out event type from name
-        split_name = name.strip().split(" - ")
-
-        type: Optional[str]
-
-        if len(split_name) > 1:
-            name = split_name[0].strip()
-            type = split_name[1].strip()
-        else:
-            type = None
-
-        return Event(name, start, end, type, teacher, location, groups)
+        return Event(name, start, end, event_type, teacher, location, groups)
